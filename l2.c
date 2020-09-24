30dcd621de020432cdd70266c80c4eb82d8e98ca
alinanasonova <47684560+alinanasonova@users.noreply.github.com>
2020-09-23 21:56:25 -0400

    size, <ls>

--- l2.c
+++ l2.c
@@ -3,7 +3,38 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <string.h>
 
+static void pf(size_t lvl, const char *t, const char *name)
+{
+    while(lvl--)
+	printf(" ");
+    printf("%s: %s\n", t, name);
+}
+
+int sss(size_t lvl, const char *dname)
+{
+    struct stat st;
+    struct dirent *ent;
+    DIR *d;
+    int c = 0;
+    char n[PATH_MAX + 1];
+    d = opendir(dname);
+    while(d && (ent = readdir(d))){
+	if(!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, ".."))
+	    continue;
+	sprintf(n, "%s/%s", dname, ent->d_name);
+	stat(n, &st);
+	if(S_ISDIR(st.st_mode)){
+	    c += sss(lvl + 1, n);
+	} else {
+	    pf(lvl + 1, "file", n);
+	    c += st.st_size;
+	}
+    }
+    if (d) closedir(d);
+    return c;
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,9 +42,13 @@ int main(int argc, char *argv[])
 	printf ("./l2 -h\n");
 	exit(1);
     }
+    
     DIR *opendir(const char *name);
     struct dirent *pdir;
     DIR *pd, *pd1;
+    struct stat st;
+    int count = 0;
+    
     char *opt = argv[1];
     if(opt[1] == '-')
 	opt[1] = 'h';
@@ -36,6 +71,14 @@ int main(int argc, char *argv[])
 		printf("%s\n", pdir->d_name);
 	    }
 	    closedir(pd);
+	    break;
+	case 's':
+	    if(argc < 3){
+		printf("./l2 -s <директория>\n");
+		break;
+	    }
+	    //printf("%s\n\n", argv[2]);
+	    printf("%d\n", sss(0, argv[2]));
 	    break;
     }
 }
