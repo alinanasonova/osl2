b5bb54b8b25f356d2a99d67139b306e7e3edfecc
alinanasonova <47684560+alinanasonova@users.noreply.github.com>
2020-09-23 20:22:50 -0400

    -l

--- l2.c
+++ l2.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+
 int main(int argc, char *argv[])
 {
     if (argc == 1){
 	printf ("./l2 -h\n");
 	exit(1);
     }
+    DIR *opendir(const char *name);
+    struct dirent *pdir;
+    DIR *pd, *pd1;
     char *opt = argv[1];
     if(opt[1] == '-')
 	opt[1] = 'h';
@@ -17,6 +25,17 @@ int main(int argc, char *argv[])
 	    printf ("Примеры запуска:\n\t./l2 -m <путь к файлу> <пункт назначения файла\n");
 	    printf ("\t./l2 -c <путь к файлу> <пункт назначениф файла>\n\t./l2 -d <путь к файлу>");
 	    printf ("\n\t./l2 -s <директория>\n\t./l2 -l <директория>\n");
-	break;
+	    break;
+	case 'l':
+	    if(argc < 3){
+		printf("./l2 -l <директория>\n");
+		break;
+	    }
+	    pd = opendir(argv[2]);
+	    while ((pdir = (readdir(pd))) != NULL){
+		printf("%s\n", pdir->d_name);
+	    }
+	    closedir(pd);
+	    break;
     }
 }
